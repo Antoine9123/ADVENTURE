07a246760c2892d083ab63addb283e72a5a800e7
Antoine-Alexandr Leytens <aa.leytens@gmail.com>
2023-11-20 22:26:27 +0100

    Add Button (select, del, refresh)

--- CHAR_MANAGER/continue_setup.py
+++ CHAR_MANAGER/continue_setup.py
@@ -12,15 +12,27 @@ class Continue(ttk.Frame):
     def create_widgets(self):
         folder_path = "GAME/personnage"
         fichiers = os.listdir(folder_path)
-
         file_names = [os.path.splitext(f)[0] for f in fichiers]
 
         self.combobox = ttk.Combobox(self, values=file_names)
         self.combobox.pack(pady=10)
         
-        button = ttk.Button(self, text="Print Selection", command=self.print_selection)
-        button.pack(pady=10)
+        select_button = ttk.Button(self, text="Select", command=self.select_action)
+        select_button.pack(side='left', padx=5)
 
-    def print_selection(self):
+        delete_button = ttk.Button(self, text="Delete", command=self.delete_action)
+        delete_button.pack(side='middle', padx=5)
+
+        refresh_button = ttk.Button(self, text="Refresh", command=self.refresh_action)
+        refresh_button.pack(side='right', padx=5)
+
+    def select_action(self):
         selected_value = self.combobox.get()
         print("Selected file:", selected_value)
+
+    def delete_action(self):
+        selected_value = self.combobox.get()
+        print("Deleted file:", selected_value)
+
+    def refresh_action(self):
+        print("Screen Refreshed")
